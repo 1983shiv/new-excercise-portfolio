ab8156b93a4fec2f0dada78f019ba1e779074490
1983shiv <1983.shiv@gmail.com>
2023-02-16 15:46:10 +0530

    revised bg of hero section

--- src/components/HomeHeroSec1.svelte
+++ src/components/HomeHeroSec1.svelte
@@ -1,5 +1,5 @@
 <!-- <section class="text-white body-font bg-gradient-to-r from-orange-500 to-gray-100"> -->
-    <section class="text-white body-font bg-orange-600">
+    <section class="text-white body-font bg-[#10243D]">
     <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 class="title-font sm:text-4xl text-6xl mb-4 font-bold text-white">Your brand on anything 
@@ -7,7 +7,7 @@
         </h1>
         <p class="mb-8 leading-relaxed text-xl">Personalised clothing is one of the best ways to promote your brand. We offer a wide range of high quality products that you can customise and get delivered to your door. It couldn’t be easier to show case your brand</p>
         <div class="flex justify-center transform hover:scale-200">
-          <button class="inline-flex font-bold bg-white text-orange-500 border-0 py-2 px-6 focus:outline-none  rounded text-lg">Ask for Quick Quote</button>
+          <button class="inline-flex font-bold bg-gray-50 text-[#10243D] hover:bg-orange-600 hover:text-white border-0 py-2 px-6 focus:outline-none  rounded text-lg">Ask for Quick Quote</button>
           </div>
       </div>
       <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
